d925a8ba5b91a7d09513fd4807451f6a1ee73daf
Fisher <george.kokinis@gmail.com>
2017-09-20 12:56:12 +0100

    Update redre.py
    Finished func, needs testing.

--- redre.py
+++ redre.py
@@ -1,8 +1,11 @@
 import praw
+import random
 
 
 def redred(strvar):
     reddit = praw.Reddit("wimgbot", 
-    user_agent="wthrbot:v100:t3rr0r_f3rr3t")
+                         user_agent="wthrbot:v100:t3rr0r_f3rr3t")
     subit = reddit.subreddit("WeatherGifs")
-    
+    pstlst = [s for s in subit.search(str(strvar))]
+    posint = random.randint(0, len(pstlst))
+    return pstlst[posint]
