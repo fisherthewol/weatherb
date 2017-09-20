a1f20778a0d4c6dde9b39a23f2ad17c0e2045902
Fisher <george.kokinis@gmail.com>
2017-09-20 12:48:19 +0100

    Fire2
    Removeed reddit code so far, moving to a new file.

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ global start
 start = True
 
 
-def redred(strvar):
-    reddit = praw.Reddit("wimgbot", 
-                         user_agent="wthrbot:v100:t3rr0r_f3rr3t")
-    subit = reddit.subreddit(
-
-
 def weatherconv(apidict):
     if "Rain" in apidict:
         return "rainy."
